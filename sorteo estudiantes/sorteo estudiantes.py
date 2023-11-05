de0110306de13a351efc7477f0dcac723d5147b2
Davidmenl11 <davidmelendres11@gmail.com>
2023-11-05 12:26:31 -0300

    subiendo el archivo sorteo estudiantes

---
+++ sorteo estudiantes/sorteo estudiantes.py
@@ -0,0 +1,10 @@
+import random
+
+nombres = ["messi", "david", "facu", "sofi", "arturo", "lauty","ahirton","tomas"]
+
+random.shuffle(nombres)
+
+
+print("sorteo:")
+for nombre in nombres:
+    print(nombre)
